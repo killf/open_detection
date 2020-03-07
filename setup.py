a81d4dd966259b0acfcecd3aa39354e5cefcc0b5
killf <killf@foxmail.com>
2020-03-07 19:27:46 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="open_detection",
-    version="0.2.1",
+    version="0.2.2",
     author="killf",
     author_email="killf@foxmail.com",
     maintainer="killf",
@@ -17,5 +17,5 @@ setup(
     install_requires=["numpy"],
     python_requires='>=3.7',
     classifiers=[],
-    keywords=[]
+    keywords=["object detection", "instance segmentation", "deep learning"]
 )
